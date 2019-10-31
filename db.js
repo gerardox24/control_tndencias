2cc38ee25a88f0baccc95de69cb983f314775267
Johnny Ramos <johnny.gerardo24@hotmail.com>
2019-10-31 13:25:35 -0500

    database connection

--- db.js
+++ db.js
@@ -1,21 +1,31 @@
 const express = require('express');
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-var dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'kimberly_clark',
-});
+// var dbConn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'kimberly_clark',
+// });
 
-dbConn.connect((err) => {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+// dbConn.connect((err) => {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
 
-    console.log('connected as id ' + dbConn.threadId);
+//     console.log('connected as id ' + dbConn.threadId);
 
-});
+// });
 
-module.exports = dbConn;
+// const sql = require('mssql');
+
+var dbConfig = {
+    user: 'susana',
+    password: 'TesisMarzo2019',
+    server: 'tendenciasnuevo.database.windows.net',
+    database: 'tendencias',
+    encrypt: true
+};
+
+module.exports = dbConfig;
--- routes/motorizados.js
+++ routes/motorizados.js
@@ -1,15 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var dbConn = require('../db');
+var dbConfig = require('../db');
+var sql = require('mssql');
 
 router.get('/', (req, res) => {
-    dbConn.query('SELECT * FROM motorizados', (error, results, fields) => {
-        if (error) throw error;
-        res.render('index', {
-            title: 'Kimberly Clark',
-            people: results
+
+    var query = 'SELECT * FROM motorizados';
+    var executeQuery = (res, query) => {
+        sql.connect(dbConfig, function(err) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                var request = new sql.Request();
+                request.query(query, function(err, results) {
+                    if (err) {
+                        console.log(err);
+                        res.send(err);
+                    } else {
+                        res.send(results);
+                        console.log(results);
+                        res.render('motorizados', {
+                            title: 'Kimberly Clark',
+                            // people: results
+                        });
+                    }
+                });
+            }
         });
-    });
+    }
+
+    console.log('hey');
+    // dbConn.query('SELECT * FROM motorizados', (error, results, fields) => {
+    //     if (error) throw error;
+    //     res.render('index', {
+    //         title: 'Kimberly Clark',
+    //         people: results
+    //     });
+    // });
+    executeQuery(res, query);
 });
 
 module.exports = router;
